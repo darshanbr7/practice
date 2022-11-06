cbdee135a1c1c29dfb52de32958a3aad0bb19d90
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-06 12:51:11 +0530

    js object

---
+++ assignment/o2.js
@@ -0,0 +1,7 @@
+let array=["name","age","agga"]
+for(i=0;i<array.length;i++){
+    console.log(array[i])
+}
+let att=new Array() // one of the array declaration method
+att.push("hello","hi")
+console.log(att)
